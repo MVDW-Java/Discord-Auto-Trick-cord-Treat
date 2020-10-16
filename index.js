8fbfc8b6887d1cb6a06b2b69cd1f2030ca439706
maxw2000 <maxvdwolf@gmail.com>
2020-10-16 13:43:25 +0200

    added type indicator and random delays.
    This makes it seem like its not a bot.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //settings
 const token = "YOUR_TOKEN_HERE";
 const channels = ["CHANNEL_ID_HERE", "ANOTHER_CHANNEL_ID_HERE"];
+var delay = 0; //ms
+const random_delay = true;
+const delay_min = 1000; //ms (Only if random_delay is true)
+const delay_max = 1000; //ms (Only if random_delay is true)
 
 
 //bot stuff
@@ -18,13 +22,21 @@ client.on("message", msg => {
 	if(msg.author.id !== '755580145078632508'){
 		return;
 	}
-	
-	if(msg.embeds[0].description.includes("h!tricks")){
-		msg.channel.send("h!tricks");
-	} else if(msg.embeds[0].description.includes("h!treats")){
-		msg.channel.send("h!treats");
+	if(random_delay){
+		delay = Math.floor(Math.random() * Math.floor(delay_max) + delay_min);
 	}
-
+	
+	if(msg.embeds[0].description.includes("h!trick")){
+		msg.channel.startTyping();
+		setTimeout(AutoClaim, delay, 'trick');
+	} else if(msg.embeds[0].description.includes("h!treat")){
+		msg.channel.startTyping();
+		setTimeout(AutoClaim, delay, 'treat');
+	}
+	function AutoClaim(type) {
+		msg.channel.send("h!" + type);
+		msg.channel.stopTyping();
+	}
 });
 
 client.login(token);
